155182ad2a9c48c627d0b1f8b6a98990cb9ab2d0
agrabauskas <agrabauskas@palantir.com>
2021-03-11 11:18:42 +0000

    rlimit

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/florianl/tc-skeleton
 
 go 1.15
 
-require github.com/iovisor/gobpf v0.1.0
+require (
+	github.com/iovisor/gobpf v0.1.0
+	golang.org/x/sys v0.0.0-20210309074719-68d13333faf2
+)
--- main.go
+++ main.go
@@ -5,25 +5,40 @@ import (
 	"time"
 
 	"github.com/iovisor/gobpf/elf"
+	"golang.org/x/sys/unix"
 )
 
 func main() {
-	mod := elf.NewModule("ebpf/kprobe_example.o")
 
-	err := mod.Load(nil)
-	if err != nil {
-		panic(err)
-	}
+		err := unix.Setrlimit(unix.RLIMIT_MEMLOCK, &unix.Rlimit{
+		Cur: unix.RLIM_INFINITY,
+		Max: unix.RLIM_INFINITY,
+		})
+		if err != nil {
+			panic(err)
+		}
 
-	err = mod.EnableKprobes(0)
-	if err != nil {
-		panic(err)
-	}
 
-	for {
-		fmt.Println("Waiting...")
-		time.Sleep(10 * time.Second)
-	}
-}
+        fmt.Println("NewModule")
+        mod := elf.NewModule("ebpf/kprobe_example")
+
+        fmt.Println("mod.Load")
+        err = mod.Load(nil)
+        if err != nil {
+                panic(err)
+        }
+
+        fmt.Println("mod.EnableKprobes")
+        err = mod.EnableKprobes(0)
+        if err != nil {
+				fmt.Println(err)
+                panic(err)
+        }
+
+        fmt.Println("Loop")
+        for {
+                fmt.Println("Waiting...")
+                time.Sleep(10 * time.Second)
+        }}
 
 
